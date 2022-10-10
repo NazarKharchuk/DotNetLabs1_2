99df19e16165baaca140e2beb72cf50def97288e
Nazar <nazariykharchuk@gmail.com>
2022-10-10 18:47:42 +0300

    Добавьте файлы проекта.

---
+++ Lab1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LinkedListLibrary;
+
+namespace Lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
